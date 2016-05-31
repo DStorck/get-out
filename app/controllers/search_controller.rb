dcd7558ceb069b0fa0e9e8129e761cbf10d26312
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-30 23:54:35 -0700

    added some logic to response because if only one event returns then it is not an array and everything gets thrown off

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,13 +5,17 @@ class SearchController < ApplicationController
 
   def display_events
     initial_response = EventfulAPIWrapper.search(params[:term])
-    response_array = initial_response["events"]["event"]
+    response_array = initial_response["events"]["event"] if initial_response["total_items"] != "0"
     @event_instances = []
-    response_array.each do |event|
-      temp = Event.find_event(event)
-      @event_instances << temp
+    if initial_response["total_items"] == '1'
+      @event_instances << Event.find_event(response_array)
+    elsif initial_response["total_items"].to_i > 1
+      response_array.each do |event|
+        temp = Event.find_event(event)
+        @event_instances << temp
+      end
     end
-    
+
     render :index
   end
 
