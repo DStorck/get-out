83c03b2fb9720872fc01501060b4af37bffe9be4
Deirdre Storck <deirdre.storck@gmail.com>
2016-05-30 23:43:07 -0700

    can search by keyword and display results with link to event website on eventful"

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,11 +4,14 @@ class SearchController < ApplicationController
   end
 
   def display_events
-    response = EventfulAPIWrapper.search(params[:term])
-    @response = response["events"]["event"]
-    # binding.pry
-
-
+    initial_response = EventfulAPIWrapper.search(params[:term])
+    response_array = initial_response["events"]["event"]
+    @event_instances = []
+    response_array.each do |event|
+      temp = Event.find_event(event)
+      @event_instances << temp
+    end
+    
     render :index
   end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,22 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+  attr_reader :name, :uid, :location, :url, :description, :when, :venue
+
+  def initialize(response)
+    @name = response["title"]
+    @uid = response["id"]
+    @location = response["city_name"]
+    @url = response["url"]
+    @description = response["description"]
+    @when = response["start_time"]
+    @venue = response["venue_name"]
+  end
+
+  def self.find_event(info)
+    self.new(info)
+
+  end
+
+
+
 end
