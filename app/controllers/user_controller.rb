06392dc2af99326f8c1e3207a2f914ce0c429f2b
Deirdre Storck <deirdre.storck@gmail.com>
2016-06-02 13:21:53 -0700

    past events now self-destruct after they have occured

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,5 @@
 class UserController < ApplicationController
 
-
   def favorite
     User.favorite(params, current_user)
     @favorites = Favorite.where(user_id: current_user.id)
@@ -30,8 +29,18 @@ class UserController < ApplicationController
   end
 
   def user_events
+    delete_past_events
     @user_events = Event.where(user_id: current_user.id).order(:start_time)
     render :add_event
   end
 
+  private
+
+  def delete_past_events
+    @user_events = Event.where(user_id: current_user.id)
+    now = Time.now.utc
+    @user_events.each do |event|
+      Event.delete(event.id) if now - event.start_time > 0
+    end
+  end
 end
