8289aa411f163f4f37a53d8d1553969bb4b71625
Deirdre Storck <deirdre.storck@gmail.com>
2016-06-03 15:22:43 -0700

    updated apiwrapper method , cleaned it up

--- lib/eventful_api_wrapper.rb
+++ lib/eventful_api_wrapper.rb
@@ -3,9 +3,8 @@ module EventfulAPIWrapper
 BASE_URL = "http://api.eventful.com/json/events/search?&app_key=PNh8dcmkrJKGX8tx&keywords="
 
   def self.search(term, current_user)
-    city = current_user.city || "Seattle"
-    @search_term = term
-    @response = HTTParty.get(BASE_URL + "#{@search_term}" + "&location="+ "#{city}", format: :json)
+    city = current_user.city
+    HTTParty.get(BASE_URL + term + "&location=#{city}", format: :json)
   end
 
 end
